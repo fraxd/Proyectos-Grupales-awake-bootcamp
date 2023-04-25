cfffbe4e63d7281c019fd266dfe3220067c36b41
fraxd <fraxd98@gmail.com>
2023-04-25 14:24:21 -0400

    update grupal 2

--- Modulo 3/Aprendizaje 2/AE 2 grupal.py
+++ Modulo 3/Aprendizaje 2/AE 2 grupal.py
@@ -1,6 +1,16 @@
-mensaje_motivador = "Francisco: weno pa la pega, Pablo: Pongale Weno, Rafael: Arriba los corazones, Patricio: Dale una hora mas metiendo mano, Franco: ponte a trabajar wn"
-print(mensaje_motivador.upper())
-lista_motivadora= mensaje_motivador.split()
 
-#buscador de compañero en la lista
-print(lista_motivadora.index("Patricio:"))
+mensaje_motivador = "Francisco: weno pa la pega, Pablo: Pongale Weno, Rafael: Arriba los corazones, Patricio: Dale una hora mas metiendo mano, Franco: ponte a trabajar wn"
+
+while True:
+    mensaje_motivador = mensaje_motivador.upper()
+    print('En mayusculas: ', mensaje_motivador)
+    lista_motivadora= mensaje_motivador.split()
+
+    #buscador de compañero en la lista
+    ## print(lista_motivadora.index("PATRICIO:"))
+
+    print('El string tiene ', len(lista_motivadora))
+    tupla_motivadora = tuple(lista_motivadora)
+    print(tupla_motivadora)
+    print('Ingrese Nuevo Mensaje Motivador:')
+    mensaje_motivador = input()
