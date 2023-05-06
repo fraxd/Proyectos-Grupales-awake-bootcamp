20b0f2d3b68dbaad6939c7b156c43dac7df0579d
MochitoSpooky <119640815+MochitoSpooky@users.noreply.github.com>
2023-05-05 22:50:16 -0400

    pdf con grafico y arrgelos en indentacion

--- Modulo 4/AE 1/AE 1 grupal.py
+++ Modulo 4/AE 1/AE 1 grupal.py
@@ -93,17 +93,22 @@ class Vendedor():
     nombre = str
     apellido = str
     seccion = str
-    __Comision = 0
     
-def __init__(self, run, nombre, apellido, seccion):
-        
-        self.run = run
-        self.nombre = nombre
-        self.apellido = apellido
-        self.seccion = seccion
+    def __init__(self, run, nombre, apellido, seccion):
+            
+            self.run = run
+            self.nombre = nombre
+            self.apellido = apellido
+            self.seccion = seccion
 
 vendedor1 = Vendedor('17888111-1', 'Pablo', 'Picasso', 'Zapatillas')
 vendedor2 = Vendedor('18999666-9', 'Vincent', 'Vangoh', 'Poleras' )
 vendedor3 = Vendedor("33333333-3", "Marcela", "Torres", "Zapatos")
 vendedor4 = Vendedor("44444444-4", "Santiago", "Sánchez", "Poleron")
-vendedor5 = Vendedor("55555555-5", "Lucía", "González", "Chaquetas")
+vendedor5 = Vendedor("55555555-5", "Lucía", "González", "Chaqueta")
+
+clientes = [juanperez, ignaciomiranda, sofiaaraya, anasanchez, carlosgomez]
+
+# Imprimir nombre y apellido de los clientes
+for cliente in clientes:
+    print(f"Nombre: {cliente.nombre}, Apellido: {cliente.apellido}")
