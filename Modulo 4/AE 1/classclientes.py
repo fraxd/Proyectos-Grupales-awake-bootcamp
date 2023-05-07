d129fb37c6bdf0f58a6c6fc1e9ea87c1b4c89f12
chesumadre <rafael.jarapro@gmail.com>
2023-05-07 18:42:35 -0400

    buscador de clientes arreglado

--- Modulo 4/AE 1/classclientes.py
+++ Modulo 4/AE 1/classclientes.py
@@ -1,5 +1,5 @@
 class Clientes():
-    idcliente = str
+    idcliente = int
     nombre = str
     apellido = str
     correo = str
@@ -7,7 +7,7 @@ class Clientes():
     __saldo = int
     
     def __init__(self, idclientes, nombre, apellido, correo, fechaderegistro, saldo):
-        self.idclientes = idclientes
+        self.idcliente = idclientes
         self.nombre = nombre
         self.apellido = apellido
         self.correo = correo
@@ -21,11 +21,11 @@ class Clientes():
     def mostrar_saldo (self):
         return self.__saldo
     
-juanperez = Clientes('juanperez', 'Juan', 'Pérez', 'juanitoxBellakito@gmail.com', '02/02/2020', 100000)
-ignaciomiranda = Clientes('ignaciomiranda', 'Ignacio', 'Miranda', 'nachito1313@gmail.com', '03/03/2020', 80000)
-sofiaaraya = Clientes('sofiaaraya', 'Sofia','Araya', 'sofiaAraya@gmail.com', '04/04/2020', 110000)
-anasanchez = Clientes("anasanchez", "Ana", "Sánchez", "anaSanchez@mail.com", "2022-01-03", 100000)
-carlosgomez = Clientes("carlosgomez", "Carlos", "Gómez", "carlosGomez@mail.com", "2022-01-03", 25000)
+juanperez = Clientes('1', 'Juan', 'Pérez', 'juanitoxBellakito@gmail.com', '02/02/2020', 100000)
+ignaciomiranda = Clientes('2', 'Ignacio', 'Miranda', 'nachito1313@gmail.com', '03/03/2020', 80000)
+sofiaaraya = Clientes('3', 'Sofia','Araya', 'sofiaAraya@gmail.com', '04/04/2020', 110000)
+anasanchez = Clientes("4", "Ana", "Sánchez", "anaSanchez@mail.com", "2022-01-03", 100000)
+carlosgomez = Clientes("5", "Carlos", "Gómez", "carlosGomez@mail.com", "2022-01-03", 25000)
 
 
 # 'id_clientes' : 1,
--- Modulo 4/AE 1/control_venta.py
+++ Modulo 4/AE 1/control_venta.py
@@ -2,17 +2,21 @@ import os
 import control_bodega
 import classclientes
 
-juanperez = classclientes.Clientes('1', 'Juan', 'Pérez', 'juanitoxBellakito@gmail.com', '02/02/2020', 100000)
-ignaciomiranda = classclientes.Clientes('2', 'Ignacio', 'Miranda', 'nachito1313@gmail.com', '03/03/2020', 80000)
-sofiaaraya = classclientes.Clientes('3', 'Sofia','Araya', 'sofiaAraya@gmail.com', '04/04/2020', 110000)
-anasanchez = classclientes.Clientes("4", "Ana", "Sánchez", "anaSanchez@mail.com", "2022-01-03", 100000)
-carlosgomez = classclientes.Clientes("5", "Carlos", "Gómez", "carlosGomez@mail.com", "2022-01-03", 25000)
+juanperez = classclientes.Clientes(1,'Juan', 'Pérez', 'juanitoxBellakito@gmail.com', '02/02/2020', 100000)
+ignaciomiranda = classclientes.Clientes(2,'Ignacio', 'Miranda', 'nachito1313@gmail.com', '03/03/2020', 80000)
+sofiaaraya = classclientes.Clientes(3,'Sofia','Araya', 'sofiaAraya@gmail.com', '04/04/2020', 110000)
+anasanchez = classclientes.Clientes(4,"Ana", "Sánchez", "anaSanchez@mail.com", "2022-01-03", 100000)
+carlosgomez = classclientes.Clientes(5,"Carlos", "Gómez", "carlosGomez@mail.com", "2022-01-03", 25000)
 
 clientes = [juanperez, ignaciomiranda, sofiaaraya, anasanchez, carlosgomez]
+
+
 
 def menu_venta():
     while True:
         control_bodega.borrarPantalla()
+        for cliente in clientes:
+            print(f"Nombre: {cliente.idcliente}, Apellido: {cliente.apellido}, Saldo:{cliente.mostrar_saldo()}")
         print('Te lo vendo SA.')
         print('Control de ventas System 1.8v')
         print('1. Ver numero de clientes')
@@ -37,8 +41,8 @@ def menu_venta():
 ## Get Cliente
 def getCliente(id_cliente):
     for cliente in clientes:
-        if int(cliente['id_clientes']) == id_cliente:
-            return cliente['nombre']
+        if cliente.idcliente == id_cliente:
+            return cliente.nombre
     return ''
 
 
