934d92c16a969b09f1441f2d01e3257f30d77d2b
chesumadre <rafael.jarapro@gmail.com>
2023-05-07 22:38:08 -0400

    classes implementadas a bodega

--- Modulo 4/AE 1/control_venta.py
+++ Modulo 4/AE 1/control_venta.py
@@ -78,10 +78,6 @@ def compras():
             print('Por defecto se pide 1.')
             stock_pedido = 1.
             break
-    autorizarCompra(stock_pedido, producto.sku)
-
-# Procesamiento de pedidos
-def autorizarCompra(stock_pedido, producto):
     print('Procesando Pedido...')
     if control_bodega.validaStock(stock_pedido, producto):
         print('Compra Aprobada.')
@@ -90,3 +86,4 @@ def autorizarCompra(stock_pedido, producto):
     else:
         print('Compra Cancelada.')
         print('Stock Insuficiente.')
+    input("press any key")
