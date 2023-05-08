23b4b6effc5e244178ae848e0267a805f3385fe3
fraxd <fraxd98@gmail.com>
2023-05-08 00:02:47 -0400

    Update

---
+++ Modulo 4/AE 2/control_bodega.py
@@ -0,0 +1,161 @@
+import os
+from classproductos import Productos
+
+zapatillanike = Productos('001', 'Nike Revolution 6', 'zapatillas','dimarsa','20', '64990')
+poleranike = Productos('002', 'Nike Sportswear', 'poleras','dimarsa','10', '19990')
+zapatosnike = Productos('003', 'Nike Air Max 90', 'zapatos','dimarsa','15', '79990')
+poleronnike = Productos('004', 'Nike Sportswear', 'poleron','dimarsa','3', '29990')
+chaquetanike = Productos('005', 'Nike Sportswear', 'chaqueta','dimarsa','5', '39990')
+guantesnike = Productos('006', 'Nike Sportswear', 'guantes','dimarsa','4', '9990')
+
+productos=[zapatillanike,poleranike,zapatosnike,poleronnike,chaquetanike,guantesnike]
+
+
+def start():
+    print('Stock Inicial establecido.')
+
+def menu_bodega():
+    while True:
+        print('\nTe lo vendo | Bodega System 2.0 \n')
+        print('1.- Agregar Nuevo Producto')
+        print('2.- Actualizar Stock producto')
+        print('3.- Ver stock actual global')
+        print('4.- Ver stock actual especifico')
+        print('5.- Ver todos los productos')
+        print('6.- Revisar productos son sobrestock')
+        print('9.- Regresar Menu principal')
+
+        opcion = int(input('\n Indique su opcion: '))
+
+        match opcion:
+            case 1:
+                borrarPantalla()
+                nombre_producto = input('Ingrese nombre nuevo producto: ')
+                while True:
+                    try:
+                        stock_producto = int(input('Ingrese stock producto: '))
+                        break
+                    except:
+                        print('Debe ingresar un numero entero.')
+                newProduct(nombre_producto, stock_producto)
+            case 2:
+                borrarPantalla()
+                stockGlobal()
+                while True:
+                    nombre_producto = input('\n Indique el nombre del producto a editar: ')
+                    while True:
+                        try:
+                            nuevo_stock = int(input('Ingrese nuevo Stock: '))
+                            break
+                        except:
+                            print('Recuerda Ingresar un valor numerico.')
+                    if updateStock(nombre_producto, nuevo_stock):
+                        break
+            #opcion 3: Ver stock actual global
+            case 3:
+                borrarPantalla()
+                stockGlobal()
+            # Opcion 4: Ver stock actual especifico
+            case 4:
+                borrarPantalla()
+                while True:
+                    nombre_producto = input('Indique producto a actualizar stock: ')
+                    if stockEspecifico(nombre_producto):
+                        break
+            # Opcion 5: Ver todos los productos'
+            case 5:
+                borrarPantalla()
+                productosGlobal()
+
+            #opcion 6:  Revisar productos son sobrestock        
+            case 6:
+                borrarPantalla()
+                while True:
+                    try:
+                        sobre_stock = int(input('Ingrese valor de sobrestock a buscar: '))
+                        break
+                    except:
+                        print('Error, Debe ingresar un numero entero. \n')
+                sobreStock(sobre_stock)
+            
+            # Opcion 9: Salir
+            case 9:
+                break
+
+
+
+## Agregar nuevos productos a la "base de datos"
+def newProduct(nombre_producto, stock):
+    productos.append({
+        'producto' : nombre_producto,
+        'stock' : stock
+    })
+    print('Producto Agregado Correctamente.')
+    return True
+
+def updateStock(nombre_producto, stock):
+    for producto in productos:
+        # Se busca el producto por nombre 
+        if producto.nombre == nombre_producto:
+            producto.stock = stock
+            print('Stock Actualizado')
+            return True
+        else:
+            print('Por algun motivo no encontramos el producto.')
+            return False
+
+#Mostrar y retornar las unidades disponibles por producto.
+def stockGlobal():
+    print('Stock de todos nuestros productos:')
+    for producto in productos:
+        print('Producto:',producto.nombre, 'Stock: ', producto.stock)        
+    return productos
+
+#Mostrar y retornar las unidades disponibles de un producto en particular.
+def stockEspecifico(nombre_producto):
+    for producto in productos:
+        if producto.nombre == nombre_producto:
+            print('Nombre: ',producto.nombre, 'Stock: ', producto.stock)
+            return producto.stock
+    print('Producto no encontrado.')
+    return False
+
+
+#Mostrar y retornar todos los productos de la tienda.
+def productosGlobal():
+    nombre_productos = []
+    print('Listado de Productos: ')
+    for producto in productos:
+        nombre_productos.append(producto.nombre)
+        print('-',producto.nombre)
+    return nombre_productos
+
+#Mostrar y retornar los productos que tienen más de un número de unidades (el usuario puede escoger el número de unidades).
+def sobreStock(sobre_stock):
+    sobre_stock_productos = []
+    for producto in productos:
+        if(int(producto.stock)>sobre_stock):
+            sobre_stock_productos.append(producto)
+            print(producto)
+    return sobre_stock_productos
+
+def getProducto(id_producto):
+    for producto in productos:
+        if int(producto.sku) == id_producto:
+            return producto
+    return False
+
+def validaStock(pedido, id_producto):
+    for producto in productos:
+        if producto.sku == id_producto:
+            if int(producto.stock)>= pedido:
+                return True
+    return False
+
+
+## Limpiar pantalla
+def borrarPantalla(): #Definimos la función estableciendo el nombre que queramos
+    if os.name == "posix":
+        os.system ("clear")
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+        os.system ("cls")
---
+++ Modulo 4/AE 2/control_venta.py
@@ -0,0 +1,93 @@
+import os
+import control_bodega
+import classclientes
+
+juanperez = classclientes.Clientes(1,'Juan', 'Pérez', 'juanitoxBellakito@gmail.com', '02/02/2020')
+ignaciomiranda = classclientes.Clientes(2,'Ignacio', 'Miranda', 'nachito1313@gmail.com', '03/03/2020')
+sofiaaraya = classclientes.Clientes(3,'Sofia','Araya', 'sofiaAraya@gmail.com', '04/04/2020')
+anasanchez = classclientes.Clientes(4,"Ana", "Sánchez", "anaSanchez@mail.com", "2022-01-03")
+carlosgomez = classclientes.Clientes(5,"Carlos", "Gómez", "carlosGomez@mail.com", "2022-01-03")
+
+clientes = [juanperez, ignaciomiranda, sofiaaraya, anasanchez, carlosgomez]
+
+
+
+def menu_venta():
+    while True:
+        control_bodega.borrarPantalla()
+        print('Te lo vendo SA.')
+        print('Control de ventas System 1.8v')
+        print('1. Ver numero de clientes')
+        print('2. Generar Pedidos')
+        print('9. Salir')
+        while True:    
+            try:
+                opcion = int(input('Ingrese su opcion: '))
+                break
+            except:
+                print('Error: Debe ingresar un numero entero.')
+        if opcion == 1:
+            printNumeroClientes()
+        elif opcion == 2:
+            compras()
+        elif opcion == 9:
+            print('\n \n \n Muchas gracias por usarnos \n \n PencaLabs ©')
+            break
+        else:
+            print('Opcion Selecionada NO Valida. ')
+
+## Get Cliente
+def getCliente(id_cliente):
+    for cliente in clientes:
+        if cliente.idcliente == id_cliente:
+            return cliente.nombre
+    return ''
+
+
+# Imprimir cantidad de clientes
+def printNumeroClientes():
+    print(' Actualmente hay registrados',len(clientes),'Clientes.')
+    input('Presione Enter para continuar.')
+
+# Armar Pedidos
+def compras():
+    while True:
+        try:
+            id = int(input('Indique N° de cliente: ')) 
+            nombre = getCliente(id)
+            if nombre != '':
+                control_bodega.borrarPantalla()
+                print('Cliente Selecionado: ', nombre )
+                break
+        except: 
+            print('Debes ingresar un numero entero.')
+    while True:
+        try:
+            id_producto = int(input('Indique Id Producto: '))
+            producto = control_bodega.getProducto(id_producto)
+            if producto:
+                print('Producto Selecionado: ', producto.nombre)
+                break
+        except:
+            print('Debe Ingresar un numero entero.')
+    while True:
+        try:
+            stock_pedido = int(input('Indique cantidad de unidades: '))
+            break
+        except:
+            print('Por defecto se pide 1.')
+            stock_pedido = 1.
+            break
+    autorizarCompra(stock_pedido, producto.sku)
+
+# Procesamiento de pedidos
+def autorizarCompra(stock_pedido, producto):
+    print('Procesando Pedido...')
+    if control_bodega.validaStock(stock_pedido, producto):
+        print('Compra Aprobada.')
+        print('Procesando Despacho.')
+        print('Gracias por Elegirnos.')
+    else:
+        print('Compra Cancelada.')
+        print('Stock Insuficiente.')
+    input('Presione enter para continuar.')
