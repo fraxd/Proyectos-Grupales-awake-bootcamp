c7011c1e350c07cb0b700ad59e0a6cfd3db161f7
chesumadre <rafael.jarapro@gmail.com>
2023-05-17 13:22:21 -0400

    comision

--- Modulo 4/AE 4/clases/classvendedores.py
+++ Modulo 4/AE 4/clases/classvendedores.py
@@ -3,6 +3,7 @@ class Vendedor():
     nombre = str
     apellido = str
     seccion = str
+    comisiones = int
     
     def __init__(self, run, nombre, apellido, seccion):
             
@@ -17,7 +18,7 @@ class Vendedor():
     def vender(self, cliente, producto, cant_Solicitada):
         valor_neto = producto.getValor_neto()
         if cliente.generarCobro(valor_neto):
-            self.__comisiones += int(valor_neto*0.005) ## 0.5%
+            self.comisiones = int(valor_neto*0.005) ## 0.5%
             producto.generarVenta(cant_Solicitada)
             print('Compra Aprobada')
         else:
